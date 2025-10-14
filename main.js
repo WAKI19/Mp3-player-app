ba3298edb72d7d637e6b0494c02dbf145d08b3de
kodai <hengqing814@gmail.com>
2025-10-14 20:36:14 +0900

    add active()&hide()

--- main.js
+++ main.js
@@ -1,89 +1,71 @@
-//要素取得
-const newPlaylistModalOpenBtn = document.getElementById("new-playlist-modal-open-btn");
-const newPlaylistModalCloseBtn = document.getElementById("new-playlist-modal-close-btn");
-const playlistList = document.getElementById("playlist-list");
-const playlistDetail = document.getElementById("playlist-detail");
-const playlistDetailCloseBtn = document.getElementById("playlist-detail-close-btn");
-
-//プレーヤー
-function openFullPlayer() {
-  const fullPlayer = document.getElementById("full-player");
-  const miniPlayer = document.getElementById("mini-player");
-
-  fullPlayer.classList.remove("hidden");
-  fullPlayer.classList.add("active");
-
-  // ミニプレーヤーは隠す
-  miniPlayer.classList.add("hidden");
+//関数
+function active(elem) {
+  elem.classList.remove("hidden");
+  elem.classList.add("active");
 }
 
-function closeFullPlayer() {
-  const fullPlayer = document.getElementById("full-player");
-  const miniPlayer = document.getElementById("mini-player");
-
-  fullPlayer.classList.remove("active");
-  fullPlayer.classList.add("hidden");
-
-  // ミニプレーヤーを表示
-  miniPlayer.classList.remove("hidden");
+function hide(elem) {
+  elem.classList.remove("active");
+  elem.classList.add("hidden");
 }
 
-//新規プレイリストモーダルを開く関数
-function openNewPlaylistModal() {
-  const newPlaylistModal = document.getElementById("new-playlist-modal");
 
-  newPlaylistModal.classList.remove("hidden");
-  newPlaylistModal.classList.add("active");
-}
-
-function closeNewPlaylistModal() {
-  const newPlaylistModal = document.getElementById("new-playlist-modal");
-
-  newPlaylistModal.classList.remove("active");
-  newPlaylistModal.classList.add("hidden");
-}
-
-//プレイリスト詳細画面を開く関数
-function openPlaylist() {
-  const playlistDetail = document.getElementById("playlist-detail");
-
-  playlistDetail.classList.remove("hidden");
-  playlistDetail.classList.add("active");
-}
-
-function closePlaylist() {
-  const playlistDetail = document.getElementById("playlist-detail");
-
-  playlistDetail.classList.remove("active");
-  playlistDetail.classList.add("hidden");
-}
-
-//プレイリストページ
-newPlaylistModalOpenBtn.addEventListener('click', () => {
-  openNewPlaylistModal();
-});
-
-newPlaylistModalCloseBtn.addEventListener('click', () => {
-  closeNewPlaylistModal();
-});
-
-playlistList.addEventListener('click', (e) => {
-  const li = e.target.closest('li');
-  if (li && playlistList.contains(li)) {
-    openPlaylist();
-  }
-});
-
-playlistDetailCloseBtn.addEventListener('click', () => {
-  closePlaylist();
-  playlistDetail.classList.add("no-animation");
-});
-
-// タブ切り替えシステム
+// イベントはここにまとめる
 document.addEventListener("DOMContentLoaded", () => {
+
+  //要素取得
+    //プレイリストページ
+  const newPlaylistModalOpenBtn = document.getElementById("new-playlist-modal-open-btn");
+  const newPlaylistModalCloseBtn = document.getElementById("new-playlist-modal-close-btn");
+  const newPlaylistModal = document.getElementById("new-playlist-modal");
+  const playlistList = document.getElementById("playlist-list");
+  const playlistDetail = document.getElementById("playlist-detail");
+  const playlistDetailCloseBtn = document.getElementById("playlist-detail-close-btn");
+
+  const miniPlayer = document.getElementById("mini-player");
+
+  const fullPlayer = document.getElementById("full-player");
+  const fullPlayerCloseBtn = document.getElementById("full-player-close-btn");
+
   const tabs = document.querySelectorAll(".tab-button");
   const contents = document.querySelectorAll(".tab-content");
 
+
+  //イベント
+    //プレイリストページ
+  newPlaylistModalOpenBtn.addEventListener('click', () => {
+    active(newPlaylistModal);
+  });
+
+  newPlaylistModalCloseBtn.addEventListener('click', () => {
+    hide(newPlaylistModal);
+  });
+
+  playlistList.addEventListener('click', (e) => {
+    const li = e.target.closest('li');
+    if (li && playlistList.contains(li)) {
+      active(playlistDetail);
+    }
+  });
+
+  playlistDetailCloseBtn.addEventListener('click', () => {
+    hide(playlistDetail);
+  });
+
+    //ミニプレーヤー
+  miniPlayer.addEventListener('click', (e) => {
+    if (e.target.id === "mini-player-play-pause-btn") return;
+    active(fullPlayer);
+    hide(miniPlayer);
+  });
+
+    //フルプレーヤー
+  fullPlayerCloseBtn.addEventListener('click', () => {
+    hide(fullPlayer);
+    active(miniPlayer);
+  });
+
+    //タブ
   tabs.forEach(tab => {
     tab.addEventListener("click", () => {
       const targetId = tab.getAttribute("data-target");
